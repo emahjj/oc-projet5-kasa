4e36a09cd31b44593955c75e8a5d13c391900de8
Emma Huj <emmahuj@live.fr>
2024-08-08 12:49:46 +0200

    Error 404

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,9 +1,15 @@
+import './Error.css'
+import { Link } from 'react-router-dom'
+import Error404 from '../../assets/404.svg'
+
 function Error() {
     return (
-        <div>
-            <h1>Oups 🙈 Cette page n'existe pas</h1>
+        <div className="error">
+            <img src={Error404} alt="Error 404" className="error__img" />
+            <h1 className="error__message">Oups! La page que vous demandez n'existe pas.</h1>
+            <Link className="error__link" to="/accueil">Retourner sur la page d’accueil</Link>
         </div>
-    )
+    )   
 }
  
 export default Error
