5e0401f14aaa37188db5c482cc4c2537bedaa678
Emma Huj <emmahuj@live.fr>
2024-08-27 20:40:32 +0200

    carousel features corrigés

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -18,22 +18,27 @@ function Carousel({pictures}) {
 
     return (
         <div className="carousel">
-          <div className="carousel__arrow carousel__arrow--left" onClick={handlePrevClick}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="80" viewBox="0 0 48 80" fill="none">
-              <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill="white"/>
-            </svg>
-          </div>  
           <div className="carousel__content">
-            <img src={pictures[currentIndex]} alt={`Slide ${currentIndex + 1}`}  />
+              <img src={pictures[currentIndex]} alt={`Slide ${currentIndex + 1}`}  />
+          </div>
+
+          <div className="carousel_features" style={{ display: pictures.length > 1 ? "block" : "none" }}>
+            <div className="carousel__arrow carousel__arrow--left" onClick={handlePrevClick}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="80" viewBox="0 0 48 80" fill="none">
+                <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill="white"/>
+              </svg>
+            </div>  
+          
             <div className="carousel__counter">
-            {currentIndex + 1}/{pictures.length}
+              {currentIndex + 1}/{pictures.length}
+            </div>
+            <div className="carousel__arrow carousel__arrow--right" onClick={handleNextClick}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="80" viewBox="0 0 48 80" fill="none">
+                <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" fill="white"/>
+              </svg>
             </div>
           </div>
-          <div className="carousel__arrow carousel__arrow--right" onClick={handleNextClick}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="80" viewBox="0 0 48 80" fill="none">
-              <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" fill="white"/>
-            </svg>
-          </div>
+
         </div>
       );
     }
