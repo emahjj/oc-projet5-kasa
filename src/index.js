226ab7d1d1acaad8dce768285a2275773ba7e4ca
Emma Huj <emmahuj@live.fr>
2024-09-21 18:01:42 +0200

    test correction

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './utils/style/main.scss';
 import './utils/style/variables.scss';
-import Home from './Pages/Home/index.jsx';
-import Fiche from './Pages/Fiche/index.jsx';
-import Information from './Pages/Infos';
+import Home from './pages/Home/index.jsx';
+import Fiche from './pages/Fiche/index.jsx';
+import Information from './pages/Infos';
 import Error from './components/Error';
 import Header from './components/Header';
 import Footer from './components/Footer';
